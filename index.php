86b2632c07aa8c0baee4da5db37006ccf39a7923
Larissa Rabello <lari.rabello@gmail.com>
2016-10-01 22:30:36 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html class="no-js" lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Projeto Desenvolvimento para Servidores II</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+</head>
+<body class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 col-lg-12 col-sm-12 col-xs-12" style="background-color:#FFD1AA; height:100px">
+			<header class="text-center" style="padding:40px">
+				<h1>Projeto Desenvolvimento para Servidores II</h1>
+			</header>
+		</div>
+	</div>
+	<div class="row images">
+		<div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+			<img class="center-block" src="http://67.media.tumblr.com/tumblr_m6mn0sUcuE1r8mh8no1_500.jpg" style="width:290px; height:290px">
+			<p class="center-block">Descrição da Imagem</p>
+			<hr>
+		</div>
+		<div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+			<img class="center-block" src="http://67.media.tumblr.com/tumblr_m6mn0sUcuE1r8mh8no1_500.jpg" style="width:290px; height:290px">
+			<p class="center-block">Descrição da Imagem</p>
+			<hr>
+		</div>
+		<div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+			<img class="center-block" src="http://67.media.tumblr.com/tumblr_m6mn0sUcuE1r8mh8no1_500.jpg" style="width:290px; height:290px">
+			<p class="center-block">Descrição da Imagem</p>
+			<hr>
+		</div>
+	</div>
+	<style type="text/css">
+		header h1 {
+			font-family: 'Quicksand', sans-serif;
+			font-size: 1.5em;
+			color:#fff;
+		}
+
+		.images {
+			padding: 20px;
+		}
+
+		hr {
+	    display: block;
+	    height: 1px;
+	    border: 0;
+	    border-top: 1px solid #ccc;
+	    margin: 1em 0;
+	    padding: 0; 
+	}
+		</style>
+	</body>
+</html>
